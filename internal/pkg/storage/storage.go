648f816dad5bd01bdd5fe4a98c3ecf57d6210175
marekpizner <m.pizner@gmail.com>
2020-09-29 22:57:05 +0200

    add storage description

--- internal/pkg/storage/storage.go
+++ internal/pkg/storage/storage.go
@@ -9,17 +9,34 @@ var (
 
 type DataType string
 
+const (
+	//StringDataType is the string data type
+	StringDataType DataType = "string"
+)
+
+//Key represents key to value
 type Key struct {
 	value    string
 	dataType DataType
 }
 
+//Val returns value of key
 func (k Key) Val() string {
 	return k.value
 }
 
+//NewStringKey creates a new key with StringDataType
+func NewStringKey(key string) Key {
+	return Key{
+		value:    key,
+		dataType: StringDataType,
+	}
+}
+
+//Value represents stored data
 type Value interface{}
 
+//Storage represents storage
 type Storage interface {
 	Put(Key, Value) error
 	Get(Key) (Value, error)
